f0f359a75e280ac776d2f8c0da9373d28dec2ae4
Petr Juna <pjuna@travelrepublic.co.uk>
2016-03-07 16:59:44 +0000

    initial commit of files

---
+++ README.md
@@ -0,0 +1,19 @@
+## System Builder Typescript Example
+
+This repository just showing how to setup [systemjs-builder](https://github.com/systemjs/builder) together with [Typescript](https://github.com/Microsoft/TypeScript). 
+
+Dependencies are tightly set to working version for better testing.
+
+## How to use this and compile it
+
+1. Install dependencies by
+
+```
+npm install
+```
+
+2. Run from root directory
+
+```
+node system.builder.js
+```
---
+++ system.builder.js
@@ -0,0 +1,29 @@
+var Builder = require("systemjs-builder");
+
+var builder = new Builder();
+
+// Collection bundling
+builder.config({
+    defaultJSExtensions: true,
+    map:{
+        "ts": "node_modules/plugin-typescript/lib/plugin.js",
+        "typescript": "node_modules/typescript/lib/typescript.js"
+    },
+    baseURL: '/',
+    transpiler: "ts",
+    typescriptOptions: {
+        module: "system",
+        target: "es5",
+        noImplicitAny: true,
+        typeCheck: true,                // also accepts "strict"
+        tsconfig: true               // also accepts a path
+    },
+    packages: {
+        application: {
+            format: 'register',
+                defaultExtension: 'ts'
+        }
+    }
+});
+builder
+    .buildStatic('./application/main', './dist/application.static.js', { globalDefs: { DEBUG: true } })
